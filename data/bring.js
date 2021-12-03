e385595a650017ecbd0adde61cbe7f516479a319
0605AbMu <71525239+0605AbMu@users.noreply.github.com>
2021-12-03 21:37:05 +0500

    Update bring.js

--- data/bring.js
+++ data/bring.js
@@ -1,10 +1,10 @@
 const app =  (require("express"))();
 const path = require("path")
 app.get("/", (req,res,next)=>{
-    res.download(path.join(__dirname, "data.json"), (err)=>{
-        //res.status(404).send({msg: "Ma'lumotlar topilmadi!"})
-    })
-    //res.status(200).sendFile(path.join(__dirname, "data.json"));
+//     res.download(path.join(__dirname, "data.json"), (err)=>{
+//         //res.status(404).send({msg: "Ma'lumotlar topilmadi!"})
+//     })
+    res.status(200).sendFile(path.join(__dirname, "data.json"));
 })
 let add = app.listen(process.env.PORT||4000, ()=>{
     
